145b8a5de1370cfd1a130285a7feab81443869d9
n3rddev <n3rddev.code@gmail.com>
2025-08-24 11:36:08 +0530

    Github username change for contribution calendar

--- Component/GithubGraph.jsx
+++ Component/GithubGraph.jsx
@@ -4,7 +4,7 @@ function GithubGraph() {
   return (
     <div style={{ width: "90%", marginTop: "2vw",padding:"2vh", position:"relative", left:"5%", border:"1px solid gray", borderRadius:"8px", bottom:"10%"}}>
       
-      <GitHubCalendar username="saket-49" />
+      <GitHubCalendar username="nerdd3v" />
     </div>
   );
 }
